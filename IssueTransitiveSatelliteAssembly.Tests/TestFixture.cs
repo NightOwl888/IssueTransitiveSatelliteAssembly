430593dafe1856640c6ae71a349f8161f4de2ef3
Shad Storhaug <shad@shadstorhaug.com>
2024-05-11 18:15:26 +0700

    Added example of transitive satellite assembly failing to copy to the output directory

---
+++ IssueTransitiveSatelliteAssembly.Tests/TestFixture.cs
@@ -0,0 +1,17 @@
+﻿using ICU4N.Impl;
+using NUnit.Framework;
+using System.Globalization;
+using Assert = NUnit.Framework.Assert;
+
+namespace IssueTransitiveSatelliteAssembly.Tests
+{
+    public class TestFixture
+    {
+        [Test]
+        public void Test()
+        {
+            var rootCulture = typeof(ICUData).Assembly.GetSatelliteAssembly(CultureInfo.InvariantCulture);
+            Assert.IsNotNull(rootCulture);
+        }
+    }
+}
